c7c86d09084ad3173afc7d8e2436e183e873d5ab
partnano // Bernhard Wick <bernhardcwick@gmail.com>
2017-07-23 22:15:57 +0200

    switched upd updateintervalls to socket.gettime()

--- client/main.lua
+++ client/main.lua
@@ -17,102 +17,106 @@ local input_serial_counter = 0
 local inputs_to_send = {}
 
 local world = {}
-local t
+local last_update_time = socket.gettime()
 
 function love.load ()
-    t = 0
-    
-    udp = socket.udp()
-    udp:settimeout(0)
-    udp:setpeername(address, port)
+   t = 0
+   
+   udp = socket.udp()
+   udp:settimeout(0)
+   udp:setpeername(address, port)
 
-    udp:send("auth")
+   udp:send("auth")
 end
 
 function love.mousepressed (x, y, button)
-    local x, y
-    
-    if love.mouse.isDown(2) then
-        x, y = love.mouse.getPosition()
-        
-        input = InputStruct:new()
+   local x, y
+   
+   if love.mouse.isDown(2) then
+      x, y = love.mouse.getPosition()
+      
+      input = InputStruct:new()
 
-        input.serial = input_serial_counter
-        input.cmd = 'move'
-        input.pos = {x = x, y = y}
+      input.serial = input_serial_counter
+      input.cmd = 'move'
+      input.pos = {x = x, y = y}
 
-        input.send_time = elapsed_time
+      input.send_time = elapsed_time
 
-        table.insert(inputs_to_send, input)
+      table.insert(inputs_to_send, input)
 
-        --finish up
-        input_serial_counter = input_serial_counter +1
-    end
+      --finish up
+      input_serial_counter = input_serial_counter +1
+   end
 end
 
 function love.update (dt)
-    -- TODO: change updaterate to sockettime
-    t = t + dt
 
-    if start_time then elapsed_time = socket.gettime() - start_time end
+   -- elapsed time is for input / action management
+   if start_time then elapsed_time = socket.gettime() - start_time end
 
-    -- send stuff
-    if t > updaterate then
-        -- is send_input AND client_id really needed here?
-        if client_id and #inputs_to_send > 0 then
-            for _, input in ipairs(inputs_to_send) do
+   -- send stuff
+   if client_id and #inputs_to_send > 0 then
+
+      -- time diff via socket time
+      -- NOTE: networking should only use sockettime
+      local _now = socket.gettime()
+      if _now - last_update_time > updaterate then
+	 print ("before loop ", _now - last_update_time, updaterate)
+	 for _, input in pairs(inputs_to_send) do
+
+	    udp:send(packer.to_string(input))
+	    
+	 end
+	 
+	 last_update_time = _now
+      end
+
+   end
+
+   repeat
+      data, msg = udp:receive()
+
+      if data then
+	 rec_data = packer.to_table(data)
+	 
+	 if rec_data.cmd == 'auth' then
+	    start_time = socket.gettime()
+	    elapsed_time = 0
+	    client_id = tonumber(rec_data.id)
+
+	    print ("authenticated, start time: ", start_time)    
             
-                udp:send(packer.to_string(input))
+	 elseif rec_data.cmd == 'ack' then
+	    -- TODO: delete acknowledged input
+
+	    for i, input in ipairs(inputs_to_send) do
+	       if input.serial == tonumber(rec_data.serial) then
+		  table.remove(inputs_to_send, i)
+		  break
+	       end
+	    end
             
-            end
-        end
+	 elseif rec_data.cmd == 'action' then
+	    -- TODO: check if values actually exist
 
-        t = t - updaterate
-    end
+	    exec_actions(rec_data.actions)
+	 else
+	    print("Unknown command: ", data.cmd)
+	 end
 
-    repeat
-        data, msg = udp:receive()
+      elseif msg ~= 'timeout' then
+	 error("Network error: " .. tostring(msg))
+      end
 
-        if data then
-            rec_data = packer.to_table(data)
-            
-            if rec_data.cmd == 'auth' then
-                start_time = socket.gettime()
-                elapsed_time = 0
-                client_id = tonumber(rec_data.id)
-
-                print ("authenticated, start time: ", start_time)    
-            
-            elseif rec_data.cmd == 'ack' then
-                -- TODO: delete acknowledged input
-
-                for i, input in ipairs(inputs_to_send) do
-                    if input.serial == tonumber(rec_data.serial) then
-                        table.remove(inputs_to_send, i)
-                        break
-                    end
-                end
-            
-            elseif rec_data.cmd == 'action' then
-                -- TODO: check if values actually exist
-
-                exec_actions(rec_data.actions)
-            else
-                print("Unknown command: ", data.cmd)
-            end
-
-        elseif msg ~= 'timeout' then
-            error("Network error: " .. tostring(msg))
-        end
-
-        ::cont::
-    until not data
+      ::cont::
+   until not data
 end
 
 local function exec_actions(actions)
-    -- TODO: stub
+   -- TODO: stub
 end
 
 function love.draw () 
-    -- TODO: stub
+   -- TODO: stub
 end
--- server/server.lua
+++ server/server.lua
@@ -5,9 +5,6 @@ local InputStruct = require 'libs.structs.input_struct'
 
 local packer = require 'libs.packer'
 
-udp:settimeout(0)
-udp:setsockname('*', 11111)
-
 local session = {}
 local players = {}
 
@@ -17,83 +14,88 @@ local entity, cmd, params
 
 local running = true
 
-local update_counter = 0
-local update_rate = 50
-
 local player_index = 0
 local start_time = nil
 local elapsed_time = nil
 
+local last_update_time = socket.gettime()
+local now = nil
+local update_rate = 0.1
+
 local input = {}
+
+udp:settimeout(0)
+udp:setsockname('*', 11111)
 
 print "beginning server loop..."
 
 while running do
-    -- ip can be ip or network message
-    -- port can be port or nil
-    data, ip, port = udp:receivefrom()
+   -- ip can be ip or network message
+   -- port can be port or nil
+   data, ip, port = udp:receivefrom()
 
-    if data and data == 'auth' and port and ip ~= 'timeout' then
+   if data and data == 'auth' and port and ip ~= 'timeout' then
 
-        -- check if player already connected
-        if session[ip] then 
-            goto cont 
-        end
-        
-        session[ip] = true;
+      -- check if player already connected
+      if session[ip] then 
+	 goto cont 
+      end
+      
+      session[ip] = true;
 
-        local new_player = {ip = ip, port = port}
-        players[player_index] = new_player
+      local new_player = {ip = ip, port = port}
+      players[player_index] = new_player
 
-        -- TODO: start as soon as enough players connected
-        -- for now this is only for a single client ... which is rather useless
-        start_time = socket.gettime()
-        elapsed_time = 0
+      -- TODO: start as soon as enough players connected
+      -- for now this is only for a single client ... which is rather useless
+      start_time = socket.gettime()
+      elapsed_time = 0
 
-        local _msg = packer.to_string({cmd = 'auth', id = player_index})
+      local _msg = packer.to_string({cmd = 'auth', id = player_index})
 
-        udp:sendto(_msg, new_player.ip, new_player.port)
-        print("Sent auth packet to ", new_player.ip, new_player.port)
-        
-        player_index = player_index +1
+      udp:sendto(_msg, new_player.ip, new_player.port)
+      print("Sent auth packet to ", new_player.ip, new_player.port)
+      
+      player_index = player_index +1
 
-        goto cont
-    end
+      goto cont
+   end
 
-    if data then
-        rec_data = packer.to_table(data)
-        
-        packer.print_table(rec_data)
+   if data then
+      rec_data = packer.to_table(data)
+      
+      packer.print_table(rec_data)
 
-        if rec_data.cmd == 'move' then
-            -- TODO: stub
-            print ("-move- packet")
-            
-            local answer = packer.to_string({cmd = 'ack', serial = rec_data.serial})
-            udp:sendto(answer, ip, port)
-        end
-    
-    elseif ip ~= 'timeout' then
-		error("Unknown network error: "..tostring(msg))
-    else
-        -- do nothing
-	end
+      if rec_data.cmd == 'move' then
+	 -- TODO: stub
+	 print ("-move- packet")
+	 
+	 local answer = packer.to_string({cmd = 'ack', serial = rec_data.serial})
+	 udp:sendto(answer, ip, port)
+      end
+      
+   elseif ip ~= 'timeout' then
+      error("Unknown network error: "..tostring(msg))
+   else
+      -- do nothing
+   end
 
-    -- periodic updates
-    -- TODO: change to sockettime
-    update_counter = update_counter +1
-    if update_counter > update_rate then
+   -- periodic updates
+   if not last_update_time then last_update_time = socket.gettime() end
+   
+   now = socket.gettime()
+   if now - last_update_time > update_rate then
 
-        for ip, port in pairs(session) do
-            -- TODO: stub
-        end
+      for ip, port in pairs(session) do
+	 -- TODO: stub
+      end
 
-        update_counter = 0
-    end
+      last_update_time = _now
+   end
 
-    ::cont::
+   ::cont::
 
-    socket.sleep(0.001)
+   socket.sleep(0.001)
 end
 
 print "server process finished"
